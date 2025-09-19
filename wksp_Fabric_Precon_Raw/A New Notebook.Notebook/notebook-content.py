9307a953231316bb07aefe7085e0ad36e937523a
Ginger Grant <ginger.grant@desertislesql.com>
2025-09-19 08:17:38 -0700

    Added New Notebook

---
+++ wksp_Fabric_Precon_Raw/A New Notebook.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ce1a42f7-5c62-4cbe-834d-ca1f4e0bab68",
+# META       "default_lakehouse_name": "gpgOpDataStore",
+# META       "default_lakehouse_workspace_id": "f097d258-1737-4f0b-8e09-09197805d11b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "ce1a42f7-5c62-4cbe-834d-ca1f4e0bab68"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# Test Notebook
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM gpgOpDataStore.dbo.iowa_liquor_sales LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
